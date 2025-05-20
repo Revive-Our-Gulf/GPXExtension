71b6660a4abee32e7e5a2d32b562096c13d68586
samuel-reedy <64679797+samuel-reedy@users.noreply.github.com>
2025-05-20 15:05:27 +1200

    Delete app/Logger/build.sh

--- app/Logger/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker stop gpx_logger
-docker remove gpx_logger
-docker build -t rogengineering/gpx_logger:latest .
-docker run -d --name gpx_logger --restart unless-stopped --network gpx_net rogengineering/gpx_logger:latest
